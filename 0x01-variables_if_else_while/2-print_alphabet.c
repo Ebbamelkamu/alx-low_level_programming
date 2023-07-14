900b3ce6c3665cdbb1c0bd0337596c05671d8052
ebbamelkamumerdasa <mawex.ebba@gmail.com>
2023-07-14 16:05:13 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Positive anything is better than negative nothing.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
