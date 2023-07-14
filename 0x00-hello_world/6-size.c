0ababce179bc98803fc4f75be24cdcc62cb1e5bc
ebbamelkamumerdasa <mawex.ebba@gmail.com>
2023-07-14 10:01:51 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A entry point
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %u bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
